c366bb7fedf1dbb2d4a2d1312f60c26eb87f3be2
Robert Herhold <reherhold@gmail.com>
2016-09-08 21:53:29 -0400

    Autocrop and scale images

--- imgur/imgur.controller.js
+++ imgur/imgur.controller.js
@@ -2,25 +2,49 @@
 
 const http = require('axios');
 const config = require('config');
-const jimp = require('jimp');
+const Jimp = require('jimp');
+
+const FULL_WIDTH = 500;
+const FULL_HEIGHT = 500;
+const imgDataPrefix = 'data:image/png;base64,';
 
 exports.init = function(router) {
     router.post('/imgur/', uploadImageToImgur);
 };
 
 function* uploadImageToImgur() {
-    const imgBuffer = new Buffer(this.request.body.image,'base64');
+    const background = new Jimp(FULL_WIDTH, FULL_HEIGHT, 0xbfd1e5ff);
 
-    jimp.read(imgBuffer).then(image => console.log(image))
+    let image = yield Jimp.read(new Buffer(this.request.body.image, 'base64'));
+    image.autocrop();
 
-    this.body = {};
+    let width = image.bitmap.width;
+    let height = image.bitmap.height;
+    
+    if(width > FULL_WIDTH || height > FULL_HEIGHT) {
+        image.scaleToFit(FULL_WIDTH, FULL_HEIGHT);
+    }
+    
+    width = image.bitmap.width;
+    height = image.bitmap.height;
 
-    // const imgurRes = yield http.post('https://api.imgur.com/3/image', this.request.body, {
-    //     headers: {
-    //         'Authorization': 'Client-ID ' + config.get('imgurClientId')
-    //     }
-    // });
-    //
-    // this.status = imgurRes.data.status;
-    // this.body = imgurRes.data.data;
+    background.composite(image, (FULL_WIDTH - width) / 2, (FULL_HEIGHT - height) / 2);
+
+    let base64Img = yield new Promise(function(resolve, reject) {
+        background.getBase64(Jimp.MIME_PNG, function(err, res) {
+            if (err) {
+                return reject(err);
+            }
+            return resolve(res);
+        });
+    });
+
+    const imgurRes = yield http.post('https://api.imgur.com/3/image', base64Img.substring(imgDataPrefix.length), {
+        headers: {
+            'Authorization': 'Client-ID ' + config.get('imgurClientId')
+        }
+    }).then(res => res.data);
+
+    this.status = imgurRes.status;
+    this.body = imgurRes.data;
 }
